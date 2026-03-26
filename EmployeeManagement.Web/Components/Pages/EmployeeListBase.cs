8be0bbd2413925c6ca621d3976d4ba543dc86cd6
vguru shetty <vgurushetty@gmail.com>
2026-03-27 00:12:15 +0530

    EmployeeManagement 1.0.0

---
+++ EmployeeManagement.Web/Components/Pages/EmployeeListBase.cs
@@ -0,0 +1,75 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.Web.Components.Pages
+{
+    public class EmployeeListBase : ComponentBase
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            LoadEmployee();
+            return base.OnInitializedAsync();
+        }
+        public void LoadEmployee()
+        {
+            Employee e1 = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "John",
+                LastName = "Ibrahim",
+                Email = "John.Ibrahim@gmail.com",
+                DateOfBirth = new DateTime(1989, 9 , 10),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 3, DepartmentName = "Physics"},
+                PhotoPath = "images/John.jpeg"
+            };
+            Employee e2 = new Employee
+            {
+                EmployeeId = 2,
+                FirstName = "Jalina",
+                LastName = "Ibrahim",
+                Email = "Jalina.Ibrahim@gmail.com",
+                DateOfBirth = new DateTime(1994, 4, 14),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 1, DepartmentName = "Biology" },
+                PhotoPath = "images/Jalina.jpeg"
+            };
+            Employee e3 = new Employee
+            {
+                EmployeeId = 3,
+                FirstName = "Mary",
+                LastName = "Jack",
+                Email = "Mary.Jack@gmail.com",
+                DateOfBirth = new DateTime(1994, 4, 14),
+                Gender = Gender.Female,
+                Department = new Department { DepartmentId = 1, DepartmentName = "Biology" },
+                PhotoPath = "images/Mary.jpeg"
+            };
+            Employee e4 = new Employee
+            {
+                EmployeeId = 4,
+                FirstName = "Goli",
+                LastName = "Patel",
+                Email = "Goli.Patel@gmail.com",
+                DateOfBirth = new DateTime(1985, 1, 19),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 2, DepartmentName = "Chemistry" },
+                PhotoPath = "images/Goli.jpeg"
+            };
+            Employee e5 = new Employee
+            {
+                EmployeeId = 5,
+                FirstName = "Sharath",
+                LastName = "Roy",
+                Email = "Sharath.Roy@gmail.com",
+                DateOfBirth = new DateTime(1991, 12, 07),
+                Gender = Gender.Male,
+                Department = new Department { DepartmentId = 4, DepartmentName = "Mathmatics" },
+                PhotoPath = "images/Sharath.jpeg"
+            };
+            Employees = new List<Employee> { e1, e2, e3, e4, e5};
+        }
+    }
+}
