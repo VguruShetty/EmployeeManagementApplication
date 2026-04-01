0b0900f943f820c5703b33e979041572db095206
vguru shetty <vgurushetty@gmail.com>
2026-04-01 23:51:02 +0530

    api

--- EmployeeManagement.WebPortal/Components/Pages/EmployeeListBase.cs
+++ EmployeeManagement.WebPortal/Components/Pages/EmployeeListBase.cs
@@ -1,19 +1,25 @@
 ﻿using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace EmployeeManagement.WebPortal.Components.Pages
 {
     public class EmployeeListBase : ComponentBase
     {
         public IEnumerable<Employee> Employees { get; set; }
+
         protected override async Task OnInitializedAsync()
         {
-            await Task.Run(LoadEmployee);
+            Employees = null;
+            await Task.Run(LoadEmployees);
+            //return base.OnInitializedAsync();
         }
 
-        public void LoadEmployee()
+        private void LoadEmployees()
         {
-            Thread.Sleep(5000);
+            System.Threading.Thread.Sleep(7000);
             Employee e1 = new Employee
             {
                 EmployeeId = 1,
@@ -22,7 +28,11 @@ namespace EmployeeManagement.WebPortal.Components.Pages
                 Email = "John.Ibrahim@gmail.com",
                 DateOfBirth = new DateTime(1989, 9, 10),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 3, DepartmentName = "Physics" },
+                Department = new Department
+                {
+                    DepartmentId = 3,
+                    DepartmentName = "Physics"
+                },
                 PhotoPath = "images/John.jpeg"
             };
             Employee e2 = new Employee
@@ -33,7 +43,11 @@ namespace EmployeeManagement.WebPortal.Components.Pages
                 Email = "Jalina.Ibrahim@gmail.com",
                 DateOfBirth = new DateTime(1994, 4, 14),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 1, DepartmentName = "Biology" },
+                Department = new Department
+                {
+                    DepartmentId = 1,
+                    DepartmentName = "Biology"
+                },
                 PhotoPath = "images/Jalina.jpeg"
             };
             Employee e3 = new Employee
@@ -44,7 +58,11 @@ namespace EmployeeManagement.WebPortal.Components.Pages
                 Email = "Mary.Jack@gmail.com",
                 DateOfBirth = new DateTime(1994, 4, 14),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 1, DepartmentName = "Biology" },
+                Department = new Department
+                {
+                    DepartmentId = 1,
+                    DepartmentName = "Biology"
+                },
                 PhotoPath = "images/Mary.jpeg"
             };
             Employee e4 = new Employee
@@ -55,7 +73,11 @@ namespace EmployeeManagement.WebPortal.Components.Pages
                 Email = "Goli.Patel@gmail.com",
                 DateOfBirth = new DateTime(1985, 1, 19),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 2, DepartmentName = "Chemistry" },
+                Department = new Department
+                {
+                    DepartmentId = 2,
+                    DepartmentName = "Chemistry"
+                },
                 PhotoPath = "images/Goli.jpeg"
             };
             Employee e5 = new Employee
@@ -66,10 +88,16 @@ namespace EmployeeManagement.WebPortal.Components.Pages
                 Email = "Sharath.Roy@gmail.com",
                 DateOfBirth = new DateTime(1991, 12, 07),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 4, DepartmentName = "Mathmatics" },
+                Department = new Department
+                {
+                    DepartmentId = 4,
+                    DepartmentName = "Mathmatics"
+                },
                 PhotoPath = "images/Sharath.jpeg"
             };
             Employees = new List<Employee> { e1, e2, e3, e4, e5 };
+
+            //StateHasChanged();
         }
 
     }
