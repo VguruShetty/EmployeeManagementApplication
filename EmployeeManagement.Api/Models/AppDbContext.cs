23f03ec5a0ff3af94fa11f770e865f880eff5007
vguru shetty <vgurushetty@gmail.com>
2026-04-09 00:10:18 +0530

    update appDbContext

--- EmployeeManagement.Api/Models/AppDbContext.cs
+++ EmployeeManagement.Api/Models/AppDbContext.cs
@@ -1,6 +1,45 @@
-﻿namespace EmployeeManagement.Api.Models
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Api.Models
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { } 
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        //seed data
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //seed data for employee
+
+            modelBuilder.Entity<Department>().HasData(
+                new Department { DepartmentId = 1, DepartmentName = "IT"});
+            modelBuilder.Entity<Department>().HasData(
+               new Department { DepartmentId = 1, DepartmentName = "HR" });
+            modelBuilder.Entity<Department>().HasData(
+               new Department { DepartmentId = 1, DepartmentName = "Payroll" });
+            modelBuilder.Entity<Department>().HasData(
+               new Department { DepartmentId = 1, DepartmentName = "Admin" });
+
+            // seed data for employee
+
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee 
+                { 
+                    EmployeeId = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Email = "JohnDoe",
+                    DateOfBirth = new DateTime(1990, 1, 1),
+                    Gender = Gender.Male,
+                    DepartmentId = 1,
+                    PhotoPath = "images/john.png"
+                });
+        }
     }
 }
