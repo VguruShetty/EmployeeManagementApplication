c5c6d3e62213b5191775cf2347db1a071114062f
vguru shetty <vgurushetty@gmail.com>
2026-04-09 23:11:46 +0530

    update repository

---
+++ EmployeeManagement.Api/Models/EmployeeRepository.cs
@@ -0,0 +1,61 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Api.Models
+{
+    public interface IEmployeeRepository
+    {
+
+    }
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public EmployeeRepository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+        public async Task<Employee> AddEmployee(Employee employee)
+        {
+            var result = await appDbContext.Employees.AddAsync(employee);
+            await appDbContext.SaveChangesAsync();
+            return result.Entity;
+        }
+        public async Task<Employee> DeleteEmployee(int id)
+        {
+            var result = await appDbContext.Employees.FirstOrDefaultAsync(e => e.EmployeeId == id);
+            if (result != null)
+            {
+                appDbContext.Employees.Remove(result);
+                await appDbContext.SaveChangesAsync();
+                return result;
+            }
+            return null;
+        }
+        public async Task<Employee> GetEmployee(int id)
+        {
+            return await appDbContext.Employees.FirstOrDefaultAsync(e => e.EmployeeId == id);
+        }
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await appDbContext.Employees.ToListAsync();
+        }
+        public async Task<Employee> UpdateEmployee(Employee employee)
+        {
+            var result = await appDbContext.Employees.FirstOrDefaultAsync(e => e.EmployeeId == employee.EmployeeId);
+            if (result != null)
+            {
+                result.FirstName = employee.FirstName;
+                result.LastName = employee.LastName;
+                result.Email = employee.Email;
+                result.DateOfBirth = employee.DateOfBirth;
+                result.Gender = employee.Gender;
+                result.DepartmentId = employee.DepartmentId;
+                result.PhotoPath = employee.PhotoPath;
+                await appDbContext.SaveChangesAsync();
+                return result;
+            }
+            return null;
+        }
+    }
+}
