99f417bdc2d069a0b1b5d097977d27da60a26965
vguru shetty <vgurushetty@gmail.com>
2026-04-12 00:17:59 +0530

    up

--- EmployeeManagement.WebPortal/Components/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.WebPortal/Components/Pages/EmployeeDetailsBase.cs
@@ -8,20 +8,39 @@ namespace EmployeeManagement.WebPortal.Components.Pages
     public class EmployeeDetailsBase : ComponentBase
     {
         public Employee Employee { get; set; } = new Employee();
-        protected string Coordinates { get; set; }
+        public string Coordinates { get; set; } = string.Empty;
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = string.Empty;
+
         [Inject]
-        public IEmployeeService EmployeeService { get; set; }
+        public IEmployeeService EmployeeService { get; set; } = null!;
 
         [Parameter]
-        public string Id { get; set; }
+        public string? Id { get; set; }
+
         protected async override Task OnInitializedAsync()
         {
             Id = Id ?? "1";
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
         }
+
         protected void Mouse_Move(MouseEventArgs e)
         {
-            Coordinates = $"X: {e.ClientX}, Y: {e.ClientY}";
+            Coordinates = $"X = {e.ClientX} Y = {e.ClientY}";
+        }
+
+        protected void Button_Click()
+        {
+            if (CssClass == "HideFooter")
+            {
+                CssClass = string.Empty;
+                ButtonText = "Hide Footer";
+            }
+            else
+            {
+                CssClass = "HideFooter";
+                ButtonText = "Show Footer";
+            }
         }
     }
 }
