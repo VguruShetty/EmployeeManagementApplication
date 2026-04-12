c83651dcb8480749234e8b81b62911c9301401ea
vguru shetty <vgurushetty@gmail.com>
2026-04-12 17:03:51 +0530

    update

---
+++ EmployeeManagement.WebPortal/Components/Pages/DatabindingDemoBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.WebPortal.Components.Pages
+{
+    public class DatabindingDemoBase : ComponentBase
+    {
+        protected string Name { get; set; } = "John Doe";
+    }
+}
