25152c71a27f6a6395faa470e91c612575254727
vguru shetty <vgurushetty@gmail.com>
2026-04-13 21:37:30 +0530

    update

--- EmployeeManagement.WebPortal/Components/Pages/EditEmployeeBase.cs
+++ EmployeeManagement.WebPortal/Components/Pages/EditEmployeeBase.cs
@@ -1,6 +1,21 @@
-﻿namespace EmployeeManagement.WebPortal.Components.Pages
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.WebPortal.Service;
+using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagement.WebPortal.Components.Pages
 {
     public class EditEmployeeBase : Component
     {
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+        [Parameter]
+        public string Id { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
     }
 }
