7a9fd952f851c6ecdd24fe1390a1c3a0c1e86d71
vguru shetty <vgurushetty@gmail.com>
2026-04-13 23:00:56 +0530

    update department controller

---
+++ EmployeeManagement.Api/Controllers/DepartmentController.cs
@@ -0,0 +1,49 @@
+﻿using EmployeeManagement.Api.Models;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using RouteAttribute = Microsoft.AspNetCore.Components.RouteAttribute;
+
+namespace EmployeeManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentRepository departmentRepository;
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            this.departmentRepository = departmentRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetDepartments()
+        {
+            try
+            {
+                return Ok(await departmentRepository.GetDepartments());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from database");
+            }
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetDepartment(int id)
+        {
+            try
+            {
+                var department = await departmentRepository.GetDepartment(id);
+                if (department == null)
+                {
+                    return NotFound();
+                }
+                return Ok(department);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from database");
+            }
+        }
+    }
+}
