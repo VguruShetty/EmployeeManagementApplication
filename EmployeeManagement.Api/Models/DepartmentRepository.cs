494f513a735403254f2d243f4724cfcf5a4c0911
vguru shetty <vgurushetty@gmail.com>
2026-04-13 22:55:02 +0530

    update department repository

--- EmployeeManagement.Api/Models/DepartmentRepository.cs
+++ EmployeeManagement.Api/Models/DepartmentRepository.cs
@@ -1,11 +1,12 @@
 ﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManagement.Api.Models
 {
     public interface IDepartmentRepository
     {
-        IEnumerable<Department> GetDepartments();
-        Department GetDepartment(int departmentId);
+        Task<IEnumerable<Department>> GetDepartments();
+        Task<Department> GetDepartment(int departmentId);
         
     }
     public class DepartmentRepository : IDepartmentRepository
@@ -15,14 +16,14 @@ namespace EmployeeManagement.Api.Models
         {
             this.appDbContext = appDbContext;
         }
-        public Department GetDepartment(int departmentId)
+        public async Task<Department> GetDepartment(int departmentId)
         {
-            return appDbContext.Departments.FirstOrDefault(d => d.DepartmentId == departmentId);
+            return await appDbContext.Departments.FirstOrDefaultAsync(d => d.DepartmentId == departmentId);
         }
 
-        public IEnumerable<Department> GetDepartments()
+        public async Task<IEnumerable<Department>> GetDepartments()
         {
-            return appDbContext.Departments.ToList();
+            return await appDbContext.Departments.ToListAsync();
         }
     }
 }
