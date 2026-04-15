a254eeb572189e03dc8e22254768ce0b407f8f7f
vguru shetty <vgurushetty@gmail.com>
2026-04-15 23:32:00 +0530

    update

--- EmployeeManagement.WebPortal/Components/Pages/CustomInputSelect.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
-
-namespace EmployeeManagement.WebPortal.Components.Pages
-{
-    public class CustomInputSelect<TValue> : InputSelect<TValue>
-    {
-
-    }
-}
