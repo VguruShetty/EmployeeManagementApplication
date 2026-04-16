da50bd6152b4391e6cb81fb9d8fdb9122d8cc63f
vguru shetty <vgurushetty@gmail.com>
2026-04-16 20:50:56 +0530

    update

--- EmployeeManagement.Models/Employee.cs
+++ EmployeeManagement.Models/Employee.cs
@@ -13,8 +13,11 @@ namespace EmployeeManagement.Models
         [Required]
         [MinLength(2)]
         public string FirstName { get; set; }
+
         [Required]
         public string LastName { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Gender Gender { get; set; }
