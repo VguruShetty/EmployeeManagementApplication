2d00e6f99ff575f8de31806fc9919a15ccd310d3
vguru shetty <vgurushetty@gmail.com>
2026-04-18 00:09:06 +0530

    update

--- EmployeeManagement.WebPortal/Models/EditEmployeeModel.cs
+++ EmployeeManagement.WebPortal/Models/EditEmployeeModel.cs
@@ -1,6 +1,25 @@
-﻿namespace EmployeeManagement.WebPortal.Models
+﻿using System.ComponentModel.DataAnnotations;
+using EmployeeManagement.Models;
+using EmployeeManagement.Models.CustomValidation;
+
+namespace EmployeeManagement.WebPortal.Models
 {
     public class EditEmployeeModel
     {
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage = "First Name is required")]
+        [MinLength(2)]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        //[EmailAddress]
+        [EmailDomainValidator(AllowedDomain = "gmail.com")]
+        public string Email { get; set; }
+        public string ConfirmEmail { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public int DepartmentId { get; set; }
+        public string PhotoPath { get; set; }
+        public Department Department { get; set; }
     }
 }
