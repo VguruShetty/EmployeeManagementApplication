6955f34606429090e6a44c73b901606bafb3d782
vguru shetty <vgurushetty@gmail.com>
2026-04-17 22:12:28 +0530

    update

--- EmployeeManagement.Models/CustomValidation/EmailDomainValidator.cs
+++ EmployeeManagement.Models/CustomValidation/EmailDomainValidator.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Models.CustomValidation
 {
-    public class EmailDomainValidator
+    public class EmailDomainValidator: ValidationAttribute
     {
     }
 }
