8df1391e8eca487b039e0bb4f39af346574f965a
vguru shetty <vgurushetty@gmail.com>
2026-04-18 21:10:22 +0530

    update

--- EmployeeManagement.Models/Employee.cs
+++ EmployeeManagement.Models/Employee.cs
@@ -11,8 +11,7 @@ namespace EmployeeManagement.Models
     public class Employee
     {
         public int EmployeeId { get; set; }
-        [Required(ErrorMessage = "First Name is required")]
-        [MinLength(2)]
+        [Required]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
