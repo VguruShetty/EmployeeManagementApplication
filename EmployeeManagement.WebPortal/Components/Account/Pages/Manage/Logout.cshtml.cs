39100e2424cf49e43dabd570a661f7c13123578b
vguru shetty <vgurushetty@gmail.com>
2026-04-24 23:03:25 +0530

    update

--- EmployeeManagement.WebPortal/Components/Account/Pages/Manage/Logout.cshtml.cs
+++ EmployeeManagement.WebPortal/Components/Account/Pages/Manage/Logout.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EmployeeManagement.WebPortal.Components.Account.Pages.Manage
@@ -6,5 +7,16 @@ namespace EmployeeManagement.WebPortal.Components.Account.Pages.Manage
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+            // This page is intentionally left blank. The actual logout logic is handled in the Logout action of the AccountController.
+        }
     }
 }
