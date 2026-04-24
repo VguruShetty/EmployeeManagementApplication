f23a803ccdff04f94f197636bb89212acae64ed0
vguru shetty <vgurushetty@gmail.com>
2026-04-24 23:47:08 +0530

    update

--- EmployeeManagement.WebPortal/Components/Account/Pages/Logout.cshtml.cs
+++ EmployeeManagement.WebPortal/Components/Account/Pages/Logout.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EmployeeManagement.WebPortal.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -8,31 +9,27 @@ namespace EmployeeManagement.WebPortal.Components.Account.Pages.Manage
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        // Use your custom user class here
+        private readonly SignInManager<EmployeeManagementWebPortalUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<EmployeeManagementWebPortalUser> signInManager, ILogger<LogoutModel> logger)
         {
             _signInManager = signInManager;
             _logger = logger;
-        }
-
-        public void OnGet()
-        {
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
             }
-            else
-            {
-                return RedirectToPage();
-            }
+
+            return RedirectToPage();
         }
     }
 }
