e7411b38d6e4437efbcf6eb3fab5555d12b6d7c9
vguru shetty <vgurushetty@gmail.com>
2026-04-24 22:55:38 +0530

    update

--- EmployeeManagement.WebPortal/Components/Account/Pages/Manage/Logout.cshtml.cs
+++ EmployeeManagement.WebPortal/Components/Account/Pages/Manage/Logout.cshtml.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace EmployeeManagement.WebPortal.Components.Account.Pages.Manage
 {
-
+    [AllowAnonymous]
     public class LogoutModel : PageModel
     {
     }
